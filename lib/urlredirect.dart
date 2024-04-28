81926fe679cc889688f572325affd8138fdf41e8
Shoaib <shoaibahmed12222234@gmail.com>
2024-04-28 23:45:49 +0530

    New commit

--- lib/urlredirect.dart
+++ lib/urlredirect.dart
@@ -1,18 +1,24 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:iu_ca/loginpage.dart';
+import 'package:flutter/widgets.dart';
+import 'package:iu_ca/loginpage.dart'; // Assuming this is your login page file
 import 'package:url_launcher/url_launcher_string.dart';
-import 'package:iconsax/iconsax.dart';
+import 'package:iconsax/iconsax.dart'; // Added the iconsax package
 
-class UrlRedirectScreen extends StatelessWidget {
+class UrlRedirectScreen extends StatefulWidget {
   const UrlRedirectScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    final user = FirebaseAuth.instance.currentUser!;
+  _UrlRedirectScreenState createState() => _UrlRedirectScreenState();
+}
 
+class _UrlRedirectScreenState extends State<UrlRedirectScreen> {
+  final user = FirebaseAuth.instance.currentUser!;
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
+      backgroundColor: Colors.black,
       appBar: AppBar(
         titleTextStyle: TextStyle(color: Colors.white, fontSize: 23),
         title: const Text('Home'),
@@ -26,26 +32,26 @@ class UrlRedirectScreen extends StatelessWidget {
               builder: (BuildContext context) {
                 return AlertDialog(
                   backgroundColor: Colors.white70,
-                  icon: Icon(Icons.verified_user, color: Colors.black),
                   title: Text(
                       'Signed in as: ${user.email},\nName: ${user.displayName}, \nVerified Email: ${user.emailVerified}'),
                   actions: [
                     TextButton(
-                        onPressed: () async {
-                          // Log the user out
-                          await FirebaseAuth.instance.signOut();
-
-                          // Navigate to the login page
-                          Navigator.pushReplacement(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) => const LoginPage(),
-                            ),
-                          );
-                        },
-                        child: const Text('Logout',
-                            selectionColor: Colors.teal,
-                            style: TextStyle(color: Colors.black))),
+                      onPressed: () async {
+                        // Log the user out
+                        await FirebaseAuth.instance.signOut();
+                        // Navigate to the login page
+                        Navigator.pushReplacement(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => const LoginPage(),
+                          ),
+                        );
+                      },
+                      child: const Text(
+                        'Logout',
+                        style: TextStyle(color: Colors.black),
+                      ),
+                    ),
                   ],
                 );
               },
@@ -54,110 +60,87 @@ class UrlRedirectScreen extends StatelessWidget {
         ),
       ),
       body: SingleChildScrollView(
-        child: Column(
+        child: Padding(
+          padding: const EdgeInsets.all(
+              20.0), // Increased padding for better spacing
+          child: Column(
+            children: [
+              _buildButton(
+                'Official Website',
+                'https://www.iul.ac.in/',
+                Colors.yellow[100]!,
+                Iconsax.global,
+              ),
+              _buildButton(
+                'Attendance',
+                'https://sms.iul.ac.in/Student/Attendance.aspx',
+                const Color.fromARGB(255, 148, 114, 103),
+                Iconsax.document,
+              ),
+              _buildButton(
+                'Timetable',
+                'https://sms.iul.ac.in/Student/ProgramWiseTimeTable.aspx',
+                const Color.fromARGB(255, 120, 162, 122),
+                Iconsax.clock,
+              ),
+              _buildButton(
+                'Coordinator List',
+                'https://drive.google.com/drive/folders/1Lmli5g69s9bwSezvDCbvHAvFglBtxB9e',
+                Colors.orange,
+                Iconsax.document_text,
+              ),
+              _buildButton(
+                'Syllabus',
+                'https://www.iul.ac.in/DepartmentsStudentZones.aspx',
+                const Color.fromARGB(255, 151, 128, 157),
+                Iconsax.book5,
+              ),
+              _buildButton(
+                'Result',
+                'https://results.iul.ac.in/StudentloginResult.aspx?exam=46&semester=0&branch=0&res=All',
+                const Color.fromARGB(255, 173, 138, 138),
+                Iconsax.receipt,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildButton(
+      String text, String url, Color backgroundColor, IconData iconData) {
+    return Padding(
+      padding:
+          const EdgeInsets.symmetric(vertical: 10.0), // Added vertical padding
+      child: ElevatedButton(
+        onPressed: () {
+          launchUrlString(url);
+        },
+        style: ElevatedButton.styleFrom(
+          backgroundColor: backgroundColor,
+          padding: const EdgeInsets.all(
+              25.0), // Increased padding for larger button size
+          minimumSize:
+              const Size(double.infinity, 160), // Increased button height
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(30.0), // Rounded corners
+          ),
+          textStyle: const TextStyle(
+              fontSize: 18,
+              color: Colors.white,
+              decorationColor: Colors.white // Set text color to white
+              ),
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: ElevatedButton.icon(
-                onPressed: () {
-                  launchUrlString('https://www.iul.ac.in/');
-                },
-                icon: const Icon(Iconsax.user_add, color: Colors.white),
-                label: const Text('Official Website'),
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: const Color.fromARGB(255, 75, 92, 100),
-                  padding: const EdgeInsets.all(67),
-                  minimumSize: const Size(double.infinity, 50),
-                  textStyle: const TextStyle(fontSize: 19, color: Colors.white),
-                ),
-              ),
+            Icon(
+              iconData,
+              color: Colors.white,
             ),
-            Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: ElevatedButton.icon(
-                onPressed: () {
-                  launchUrlString(
-                      'https://sms.iul.ac.in/Student/Attendance.aspx');
-                },
-                icon: const Icon(Iconsax.calendar1, color: Colors.white),
-                label: const Text('Attendance'),
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: const Color.fromARGB(255, 83, 24, 6),
-                  padding: const EdgeInsets.all(67),
-                  minimumSize: const Size(double.infinity, 50),
-                  textStyle: const TextStyle(fontSize: 19, color: Colors.white),
-                ),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: ElevatedButton.icon(
-                onPressed: () {
-                  launchUrlString(
-                      'https://sms.iul.ac.in/Student/ProgramWiseTimeTable.aspx');
-                },
-                icon: const Icon(Iconsax.timer_start, color: Colors.white),
-                label: const Text('Timetable'),
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: const Color.fromARGB(255, 12, 59, 14),
-                  padding: const EdgeInsets.all(67),
-                  minimumSize: const Size(double.infinity, 50),
-                  textStyle: const TextStyle(fontSize: 19, color: Colors.white),
-                ),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: ElevatedButton.icon(
-                onPressed: () {
-                  launchUrlString(
-                      'https://drive.google.com/drive/folders/1Lmli5g69s9bwSezvDCbvHAvFglBtxB9e');
-                },
-                icon: const Icon(Iconsax.people, color: Colors.white),
-                label: const Text('Coordinator List'),
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.orange,
-                  padding: const EdgeInsets.all(67),
-                  minimumSize: const Size(double.infinity, 50),
-                  textStyle: const TextStyle(fontSize: 19, color: Colors.white),
-                ),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: ElevatedButton.icon(
-                onPressed: () {
-                  launchUrlString(
-                      'https://www.iul.ac.in/DepartmentsStudentZones.aspx');
-                },
-                icon: const Icon(Iconsax.book_square, color: Colors.white),
-                label: const Text('Syllabus'),
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: const Color.fromARGB(255, 61, 40, 65),
-                  minimumSize: const Size(double.infinity, 50),
-                  padding: const EdgeInsets.all(67),
-                  textStyle: const TextStyle(fontSize: 19, color: Colors.white),
-                ),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: ElevatedButton.icon(
-                onPressed: () {
-                  launchUrlString(
-                      'https://results.iul.ac.in/StudentloginResult.aspx?exam=46&semester=0&branch=0&res=All');
-                },
-                icon:
-                    const Icon(Iconsax.percentage_square4, color: Colors.white),
-                label: const Text('Result'),
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: const Color.fromARGB(255, 69, 53, 53),
-                  padding: const EdgeInsets.all(67),
-                  minimumSize: const Size(double.infinity, 50),
-                  textStyle: const TextStyle(fontSize: 19, color: Colors.white),
-                ),
-              ),
-            ),
+            const SizedBox(width: 10),
+            Text(text),
           ],
         ),
       ),
