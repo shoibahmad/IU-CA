5614cebbe6934a4299b38e75847656c7376b4a8f
Shoaib <shoaibahmed12222234@gmail.com>
2024-05-13 19:11:02 +0530

    New Commit

--- lib/adminscreen.dart
+++ lib/adminscreen.dart
@@ -4,9 +4,8 @@ import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:iconsax/iconsax.dart';
+import 'package:iu_ca/urlredirect.dart'; // Replace with your actual urlredirect.dart path
 import 'dart:convert';
-
-import 'package:iu_ca/urlredirect.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 class AdminScreen extends StatefulWidget {
@@ -21,31 +20,38 @@ class _AdminScreenState extends State<AdminScreen> {
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
   final _titleController = TextEditingController();
   final _bodyController = TextEditingController();
+  bool _showNotifications = false;
 
   @override
   void initState() {
     super.initState();
     _requestPermissionsAndGetToken();
     _checkUserAccess();
+    FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+      _storeNotificationInFirestore(
+          message.notification?.title ?? '', message.notification?.body ?? '');
+    });
   }
 
   Future<void> _checkUserAccess() async {
     User? user = _auth.currentUser;
     if (user == null || user.email != 'hodca@iul.ac.in') {
-      // Access denied - show alert and redirect
+      // Show the alert dialog and prevent dismissal until OK is pressed
       await showDialog(
         context: context,
+        barrierDismissible: false, // Prevent closing by tapping outside
         builder: (context) => AlertDialog(
           title: const Text('Access Denied'),
-          content:
-              const Text('You do not have permission to access this page.'),
+          content: const Text('You do not have permission to access this page.'),
           actions: [
             TextButton(
               onPressed: () {
-                Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: ((context) => UrlRedirectScreen())));
+                Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => const UrlRedirectScreen(),
+                  ),
+                );
               },
               child: const Text('OK'),
             ),
@@ -66,7 +72,6 @@ class _AdminScreenState extends State<AdminScreen> {
       provisional: false,
       sound: true,
     );
-
     if (settings.authorizationStatus == AuthorizationStatus.authorized) {
       print('User granted permission');
       String? token = await messaging.getToken();
@@ -84,12 +89,25 @@ class _AdminScreenState extends State<AdminScreen> {
 
   Future<void> _storeTokenInFirestore(String token) async {
     try {
-      await _firestore.collection('users').doc(_auth.currentUser!.uid).set({
-        'token': token,
-      }, SetOptions(merge: true));
+      await _firestore
+          .collection('users')
+          .doc(_auth.currentUser!.uid)
+          .set({'token': token}, SetOptions(merge: true));
       print('Token stored successfully');
     } catch (e) {
       print('Error storing token: $e');
+    }
+  }
+
+  Future<void> _storeNotificationInFirestore(String title, String body) async {
+    try {
+      await _firestore.collection('notifications').add({
+        'title': title,
+        'body': body,
+        'timestamp': FieldValue.serverTimestamp(),
+      });
+    } catch (e) {
+      print('Error storing notification: $e');
     }
   }
 
@@ -98,9 +116,23 @@ class _AdminScreenState extends State<AdminScreen> {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Admin Panel'),
+        titleTextStyle: const TextStyle(color: Colors.black, fontSize: 23),
         automaticallyImplyLeading: false,
         backgroundColor: Colors.white,
         centerTitle: true,
+        actions: [
+          IconButton(
+            onPressed: () {
+              setState(() {
+                _showNotifications = !_showNotifications;
+              });
+              if (_showNotifications) {
+                _showNotificationsBottomSheet(context);
+              }
+            },
+            icon: const Icon(Iconsax.notification, color: Colors.black),
+          ),
+        ],
       ),
       body: Center(
         child: Padding(
@@ -177,13 +209,14 @@ class _AdminScreenState extends State<AdminScreen> {
       floatingActionButton: FloatingActionButton(
         onPressed: () async {
           const url =
-              'https://drive.google.com/drive/folders/1Lmli5g69s9bwSezvDCbvHAvFglBtxB9e'; // Replace with your desired URL
-          // ignore: deprecated_member_use
-          if (await canLaunch(url)) {
-            // ignore: deprecated_member_use
-            await launch(url);
-          } else {
-            throw 'Could not launch $url';
+              'https://drive.google.com/drive/folders/1Lmli5g69s9bwSezvDCbvHAvFglBtxB9e';
+          try {
+            await launchUrl(Uri.parse(url));
+          } catch (e) {
+            // Handle URL launch errors
+            print('Error launching URL: $e');
+            ScaffoldMessenger.of(context).showSnackBar(
+                SnackBar(content: Text('Failed to open URL: $e')));
           }
         },
         child: const Icon(
@@ -194,48 +227,108 @@ class _AdminScreenState extends State<AdminScreen> {
         hoverColor: Colors.amber,
         splashColor: Colors.black,
       ),
-      floatingActionButtonLocation:
-          FloatingActionButtonLocation.endFloat, // Position at bottom right
+      floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
+    );
+  }
+
+  void _showNotificationsBottomSheet(BuildContext context) {
+    showModalBottomSheet(
+      context: context,
+      shape: const RoundedRectangleBorder(
+        borderRadius: BorderRadius.only(
+          topLeft: Radius.circular(20),
+          topRight: Radius.circular(20),
+        ),
+      ),
+      builder: (context) => Container(
+        height: MediaQuery.of(context).size.height * 0.7,
+        decoration: BoxDecoration(
+          color: Colors.grey[200],
+          borderRadius: const BorderRadius.only(
+            topLeft: Radius.circular(20),
+            topRight: Radius.circular(20),
+          ),
+        ),
+        child: StreamBuilder<QuerySnapshot>(
+          stream: _firestore
+              .collection('notifications')
+              .orderBy('timestamp', descending: true)
+              .snapshots(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const Center(child: CircularProgressIndicator());
+            } else if (snapshot.hasError) {
+              return Text('Error: ${snapshot.error}');
+            }
+            final notifications = snapshot.data!.docs;
+            return ListView.builder(
+              itemCount: notifications.length,
+              itemBuilder: (context, index) {
+                final notification =
+                    notifications[index].data() as Map<String, dynamic>;
+                return ListTile(
+                  title: Text(notification['title']),
+                  subtitle: Text(notification['body']),
+                  
+                );
+              },
+            );
+          },
+        ),
+      ),
     );
   }
 
   Future<void> _sendNotificationToAllUsers() async {
+    // Check if title and body are empty
+    if (_titleController.text.isEmpty || _bodyController.text.isEmpty) {
+      ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(content: Text('Notification title and body cannot be empty!')));
+      return;
+    }
     try {
       QuerySnapshot usersSnapshot = await _firestore.collection('users').get();
       for (QueryDocumentSnapshot userSnapshot in usersSnapshot.docs) {
-        String userToken = userSnapshot.get('token');
-        await http.post(
-          Uri.parse('https://fcm.googleapis.com/fcm/send'),
-          headers: <String, String>{
-            'Content-Type': 'application/json',
-            'Authorization':
-                'key=AAAAUaxXpVc:APA91bEiVTOQufrhtrXNHXUaV2JVzg8sIs1t-fYIoeH8_WKoW-KQ6d3xUmP64JYbEmdstTYiUNDJz7o9w1hokRIb1tyRM0_7V1SSycs640EYT4dHffR79DR0TwCvLYNPl7D9_tpgjKw7',
-          },
-          body: jsonEncode(
-            <String, dynamic>{
-              'notification': <String, dynamic>{
-                'body': _bodyController.text,
-                'title': _titleController.text,
-              },
-              'priority': 'high',
-              'data': <String, dynamic>{
-                'click_action': 'FLUTTER_NOTIFICATION_CLICK',
-                'id': '1',
-                'status': 'done',
-              },
-              'to': userToken,
+        Map<String, dynamic> userData = userSnapshot.data() as Map<String, dynamic>;
+        if (userData.containsKey('token')) {
+          String userToken = userData['token'];
+          // Send notification to one user at a time
+          await http.post(
+            Uri.parse('https://fcm.googleapis.com/fcm/send'),
+            headers: <String, String>{
+              'Content-Type': 'application/json',
+              'Authorization':
+                  'key=AAAAUaxXpVc:APA91bEiVTOQufrhtrXNHXUaV2JVzg8sIs1t-fYIoeH8_WKoW-KQ6d3xUmP64JYbEmdstTYiUNDJz7o9w1hokRIb1tyRM0_7V1SSycs640EYT4dHffR79DR0TwCvLYNPl7D9_tpgjKw7', // Replace with your actual server key
             },
-          ),
-        );
+            body: jsonEncode(
+              <String, dynamic>{
+                'notification': <String, dynamic>{
+                  'body': _bodyController.text,
+                  'title': _titleController.text,
+                },
+                'priority': 'high',
+                'data': <String, dynamic>{
+                  'click_action': 'FLUTTER_NOTIFICATION_CLICK',
+                  'id': '1',
+                  'status': 'done',
+                },
+                'to': userToken,
+              },
+            ),
+          );
+          _storeNotificationInFirestore(_titleController.text, _bodyController.text);
+          // Send only one notification at a time
+          break; 
+        } else {
+          print("User document missing 'token' field: ${userSnapshot.id}");
+        }
       }
-      ScaffoldMessenger.of(context).showSnackBar(
-        const SnackBar(content: Text('Notification sent!')),
-      );
+      ScaffoldMessenger.of(context)
+          .showSnackBar(const SnackBar(content: Text('Notification sent!')));
     } catch (e) {
       print('Error sending notification: $e');
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(content: Text('Failed to send notification: $e')),
-      );
+      ScaffoldMessenger.of(context)
+          .showSnackBar(SnackBar(content: Text('Failed to send notification: $e')));
     }
   }
 }
